8189a6a8b34c4bb35f1750bcc6322c83e729ba13
Jose María <jmcr2500@gmail.com>
2020-04-15 14:07:29 +0200

    02.Salas propias implementadas

--- Game.java
+++ Game.java
@@ -19,7 +19,7 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
-        
+
     /**
      * Create the game and initialise its internal map.
      */
@@ -34,23 +34,27 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
-      
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        
-        // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        Room salaPrincipal, sotano, prision, capilla, torreon, salaDelTrono, salaDelTesoro, fuenteDeLaSabiduria;
 
-        currentRoom = outside;  // start game outside
+        // create the rooms
+        salaPrincipal = new Room("in front of the entrance");
+        sotano = new Room("on a dark basement");
+        prision = new Room("on the scary prisión");
+        capilla = new Room("on the disturbing chapel");
+        torreon = new Room("on a decrepit tower");
+        salaDelTrono = new Room("on the majestic throne room");
+        salaDelTesoro = new Room("on a filled with gold treasure room");
+
+        // initialise room exits
+        salaPrincipal.setExits(null, capilla, null, sotano);
+        sotano.setExits(null, salaPrincipal, prision, null);
+        prision.setExits(sotano, null, null, null);
+        capilla.setExits(null, torreon, salaDelTrono, salaPrincipal);
+        torreon.setExits(null, null, null, capilla);
+        salaDelTrono.setExits(capilla, null, salaDelTesoro, null);
+        salaDelTesoro.setExits(salaDelTrono, null, null, null);
+
+        currentRoom = salaPrincipal;  // start game outside
     }
 
     /**
@@ -62,7 +66,7 @@ public class Game
 
         // Enter the main command loop.  Here we repeatedly read commands and
         // execute them until the game is over.
-                
+
         boolean finished = false;
         while (! finished) {
             Command command = parser.getCommand();
